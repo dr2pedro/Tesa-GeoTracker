ea7040edb27559cc80a13f64dd9b70b7e37e3aa0
Pedro (W) <dr2p@hotmail.com>
2020-06-12 23:50:22 -0300

    desistindo da doubles_list

--- polygons_engine/get_polygons.R
+++ polygons_engine/get_polygons.R
@@ -1,6 +1,6 @@
 
 get_polygons <- 
-function(regioes, alias_list=NULL, doubles_list=NULL) {
+function(regioes, alias_list=NULL) {
   
   if(!require(jsonlite)) {
     install.packages("jsonlite")
@@ -15,36 +15,12 @@ function(regioes, alias_list=NULL, doubles_list=NULL) {
     install.packages("sp")
   }
 
-nomes <- unique(regioes)
+nomes <- toupper(unique(regioes))
 
-if(length(nomes)!=regioes) {
-  warning("Existem nomes repetidos no array. Os nomes que estiverem na 'doubles_list.csv' serão trocados segundo critério 
-  estabelecido na função, caso contrário a função apresentará erro.")
+  if(length(nomes)!=length(regioes))
+  stop("Existem nomes repetidos no array. Esses nomes devem ser substituidos pelo nome (referência da divisão geográfica superior), por exemplo:\n\nMESQUITA (RIO DE JANEIRO)\nMESQUITA (MINAS GERAIS).\n
+       Lembre-se de incluir os nomes desses municípios na alias_list caso o polígono não seja retornado corretamente.")
 
-if(!is.null(doubles_list)){
-      
-      if(is.data.frame(doubles_list)!=TRUE)
-      stop("O objeto escolhido como doubles_list não é um data.frame.")
-
-      if(ncol(doubles_list)<3)
-      stop("Tem que ter três colunas no data.frame: uma contendo o nome real e duas contendo os nomes a serem trocados.\n 
-      O ideal para os codinomes é inserir uma indicação da divisão geográfica acima, por exemplo: \n
-      para o município de Mesquista um nome poderia ser 'MESQUITA RIO' e o outro 'MESQUITA MINAS'.")
-
-      if(is.character(doubles_list[,1:ncol(doubles_list)])!=TRUE)
-      stop("Alguma coluna não é de caractéres.")
-
-      doubles_list = doubles_list
-
-      nomes <- ifelse(nomes %in% doubles_list[,1]==TRUE, doubles_list[,2], nomes)
-
-      
-      } else {
-        doubles_list <- read.csv('doubles_list.csv')
-        nomes <- ifelse(nomes %in% doubles_list[,1]==TRUE, doubles_list[,2], nomes)
-      }
-
-}
 
   # a ideia da alias_list é ter um data.frame controle no qual quando determinado nome aparece que sabidamente não vai retornar o polígono
   # desejado ele será trocado por um nome que retorne o polígono correto. Caso o usuário tenha a sua lista ele pode usar,
@@ -59,17 +35,22 @@ if(!is.null(doubles_list)){
       if(ncol(alias_list)!=2)
       stop("tem que ter duas colunas no data.frame: uma contendo o nome real e uma contendo o nome a ser trocado")
 
+      alias_list = alias_list
+      
       if(is.character(alias_list[,1])!=TRUE)
-      stop("a primeira coluna não é de caractéres")
+      alias_list[,1]=as.character(alias_list[,1])
 
       if(is.character(alias_list[,2])!=TRUE)
-      stop("a segunda coluna não é de caractéres")
-
-      alias_list = alias_list
+      alias_list[,2]=as.character(alias_list[,2])
+      
 
       nomes <- ifelse(nomes %in% alias_list[,1]==TRUE, alias_list[,2], nomes)
       } else {
-        alias_list <- read.csv('alias_list.csv')
+        alias_list <- read.csv('alias_list.csv', header = FALSE)
+        
+        alias_list[,1]=as.character(alias_list[,1])
+        alias_list[,2]=as.character(alias_list[,2])
+        
         nomes <- ifelse(nomes %in% alias_list[,1]==TRUE, alias_list[,2], nomes)
       }
 
